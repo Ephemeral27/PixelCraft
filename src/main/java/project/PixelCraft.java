f8739da6debf28564e0abe28ae19e1eb0c3ea903
Ephemeral <hyett479@gmail.com>
2026-03-28 17:01:52 -0400

    Update PixelCraft.java

--- src/main/java/project/PixelCraft.java
+++ src/main/java/project/PixelCraft.java
@@ -22,7 +22,7 @@ public class PixelCraft {
 
         // Ensure that a converter name and a filename has been provided
         if (args.length < 2) {
-            System.out.println("Usage: java -cp \"path/to/classes\" PixelCraft <ConverterName/Greyscale/Invert/Rotate/Blur/Flip/Pixelate> <image_file.png>");
+            System.out.println("Usage: java -cp \"path/to/classes\" PixelCraft <ConverterName/Greyscale/Invert/Rotate/Blur/Pixelate/Sepia> <image_file.png>");
             System.exit(1);
         }
 
@@ -49,8 +49,8 @@ public class PixelCraft {
             else if (converterName.equals("Flip")) {
                 outputImage = Flip.toFlip(inputImage);
             }
-            else if (converterName.equals("Pixelate")) {
-                outputImage = Pixelate.toPixelate(inputImage);
+            else if (converterName.equals("Sepia")) {
+                outputImage = Sepia.toSepia(inputImage);
             }
             else {
                 System.out.println("Error: Unknown converter name.");
